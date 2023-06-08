37f2e857f727a3cfba4c96791934d2306047aff9
joshii04 <joshua.sinclair04@gmail.com>
2023-06-08 15:09:51 +0100

    done
    very simple makeup of it. Conclusion, I would rather learn the parallax effect.

--- js/animations.js
+++ js/animations.js
@@ -1,15 +1,54 @@
-var controller = new ScrollMagic.Controller();
+const html = document.documentElement;
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
+const info = document.querySelector('.info');
 
-// Get all sections
-var sections = document.querySelectorAll('.scroll-section');
+const frameCount = 6;
+const currentFrame = index => (
+  `img/image${index.toString().padStart(3, '0')}.jpg`
+)
 
-// For each section
-for (var i = 0; i < sections.length; i++) {
-    // Create scene
-    new ScrollMagic.Scene({
-        triggerElement: sections[i], // trigger CSS animation when this is in view
-        triggerHook: 0.5 // middle of the viewport
-    })
-    .setClassToggle(sections[i], 'active') // add class to project
-    .addTo(controller); // add Scene to ScrollMagic Controller
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+    img.onload = function(){
+      if(i == 1){
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      }
+    }
+  }
+};
+
+const img = new Image()
+
+const updateImage = index => {
+  img.src = currentFrame(index);
+  img.onload=function(){
+    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+  }
 }
+
+window.addEventListener('scroll', () => {  
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+  
+  const infoRect = info.getBoundingClientRect();
+  if (infoRect.bottom <= 0) {
+    requestAnimationFrame(() => updateImage(frameIndex + 1))
+  }
+});
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
+preloadImages();
